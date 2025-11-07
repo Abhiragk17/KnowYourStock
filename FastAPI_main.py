bbf3feeebaa403ee6c66fd1b53d7b5265ce38649
abhiragk17 <abhiragkulkarni12@gmail.com>
2025-11-07 19:10:53 +0530

    changed the host url

--- FastAPI_main.py
+++ FastAPI_main.py
@@ -191,4 +191,4 @@ if __name__ == "__main__":
     # Run via: python FastAPI_main.py (for quick local tests)
     import uvicorn
 
-    uvicorn.run("FastAPI_main:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("FastAPI_main:app", host="0.0.0.0", port=8000, reload=False)
